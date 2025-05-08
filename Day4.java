a335b1db3e04c53db87d4c3a9bc409ecfc21a055
YakshPatel0805 <yakshpatel6464@gmail.com>
2025-05-08 10:33:14 +0530

    Day-4

---
+++ Day4.java
@@ -0,0 +1,44 @@
+
+
+class Day4{
+    public static void main(String args[]){
+        
+        // Enhanced Switch Statement, for loop, Ternary Operator
+
+        // int n = 8;
+        // switch(n){
+        //     case 1 -> System.out.print("Monday");
+        //     case 2 -> System.out.print("Tuesday");
+        //     case 3 -> System.out.print("Wednesday");
+        //     case 7 -> System.out.print("Sunday");
+        //     default -> System.out.print("Invalid Day");
+        // }
+        
+        // int[] arr = {1,2,3,4,5};
+        // for (int var: arr){
+        //     System.out.print(var);
+        // }
+        
+        // int n = 5;
+        // String x = (n == 5) ? "Yes" : "No";
+        // System.out.print(x);
+
+
+
+        // sum of digits of a number until it is a single digit
+        // int n = 789;
+        // int sum = 0;
+        // while(n > 9){
+        //     sum = 0;
+        //     int rem;
+        //     while(n != 0){
+        //         rem = n % 10;
+        //         sum = sum + rem;
+        //         n = n / 10;
+        //     }
+        //     n = sum;
+        // }
+        // System.out.print(sum);
+        
+    }
+}
