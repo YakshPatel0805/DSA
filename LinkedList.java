e8cd29c837af18cb3928c202234c60d9777f7465
YakshPatel0805 <yakshpatel6464@gmail.com>
2025-05-28 10:34:17 +0530

    LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,15 @@
 class Main{
 
-    // static Node convertArrayToLinkedList(int[] arr){
-    //     if(arr.length == 0) return null;
-    //     Node head = new Node(arr[0]);
-    //     Node current = head;
-    //     for(int i = 1; i < arr.length; i++){
-    //         current.next = new Node(arr[i]);
-    //         current = current.next;
-    //     }
-    //     return head;
-    // }
+    static Node convertArrayToLinkedList(int[] arr){
+        if(arr.length == 0) return null;
+        Node head = new Node(arr[0]);
+        Node current = head;
+        for(int i = 1; i < arr.length; i++){
+            current.next = new Node(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
 
     static void printLinkedList(Node head){
         Node current = head;
@@ -112,9 +112,190 @@ class Main{
         return head;
     }
 
+
+    // finding loop in LinkedList
+    static boolean hasLoop(Node head){
+        if (head == null) return false;
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+
+    // staarting point of loop in LinkedList
+    static Node findLoopStart(Node head){
+        if (head == null) return null;
+        Node slow = head;
+        Node fast = head;
+
+        // Detect loop
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+
+        if (fast == null || fast.next == null) return null;
+
+        // Find the start of the loop
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+
+    // count nodes in a loop in LinkedList
+    static int countNodesInLoop(Node head){
+        if (head == null) return 0;
+        Node slow = head;
+        Node fast = head;
+
+        // Detect loop
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+
+        if (fast == null || fast.next == null) return 0;
+
+        // Count nodes in the loop
+        int count = 1;
+        Node current = slow.next;
+        while(current != slow){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+
+    // Finding middle element in LinkedList
+    static Node findMiddle(Node head){
+        if (head == null) return null;
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // delete loop in LinkedList
+    static Node deleteLoop(Node head){
+        if (head == null) return null;
+        Node slow = head;
+        Node fast = head;
+        boolean loopExists = false;
+
+        // Detect loop
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                loopExists = true;
+                break;
+            }
+        }
+
+        if (!loopExists) return head; 
+
+        // Find the start of the loop
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // Find the last node in the loop and break it
+        Node loopStart = slow;
+        while(fast.next != loopStart){
+            fast = fast.next;
+        }
+        fast.next = null;
+        return head;
+    }
+
+    // find length of LinkedList
+    static int get_length(Node head){
+        int length = 0;
+        Node current = head;
+        while(current != null){
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+    // delete node at position from back in LinkedList
+    // static Node deleteNodeAtPositionFromBack(Node head, int position){
+    //     int startLength = get_length(head) - position + 1;
+    //     if (head == null) return null;
+    //     if (startLength == 1){
+    //         head = head.next;
+    //         return head;
+    //     }
+    //     else{
+    //         Node prev = head;
+    //         Node temp = head;
+    //         while(startLength != 1){
+    //             prev = temp;
+    //             temp = temp.next;
+    //             startLength--;
+    //         }
+    //         prev.next = temp.next;
+    //         temp.next = null;
+    //         return head;
+    //     }
+    // }
+
+
+    // segregate even and odd nodes in LinkedList
+    static Node segregateEvenOdd(Node head){
+        if (head == null) return null;
+        Node evenHead = null, evenTail = null;
+        Node oddHead = null, oddTail = null;
+
+        Node current = head;
+        while(current != null){
+            if (current.data % 2 == 0){
+                if (evenHead == null){
+                    evenHead = current;
+                    evenTail = evenHead;
+                } else {
+                    evenTail.next = current;
+                    evenTail = evenTail.next;
+                }
+            } else {
+                if (oddHead == null){
+                    oddHead = current;
+                    oddTail = oddHead;
+                } else {
+                    oddTail.next = current;
+                    oddTail = oddTail.next;
+                }
+            }
+            current = current.next;
+        }
+
+        if (evenTail != null) evenTail.next = oddHead;
+        if (oddTail != null) oddTail.next = null;
+
+        return evenHead != null ? evenHead : oddHead;
+    }
+
     public static void main(String[] args) {
         // int[] arr = {1, 2, 3, 4, 5};
         // Node linkedList = convertArrayToLinkedList(arr);
+
         Node linkedList = null;
         linkedList = insertAtEnd(linkedList, 1);
         linkedList = insertAtEnd(linkedList, 2);
@@ -123,7 +304,6 @@ class Main{
         linkedList = insertAtEnd(linkedList, 5);
         System.out.println("Original Linked List:");
         printLinkedList(linkedList);
-
 
     }
 }
